1f45ef805f6a123362775c8ac09a273ff31e693d
Dawid Michalowicz <dmichalowicz96@gmail.com>
2018-10-21 23:46:49 +0200

    error handling on login page

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,16 +1,27 @@
 <template>
     <v-container fill-height>
         <v-layout align-center justify-center>
-            <v-flex xs6>
-                <h3 class="headline">Zaloguj się</h3>
+
+            <v-flex xs12 sm6>
+                <v-alert class="my-4" :value="hasErrors" type="error" @click="closeAlert" transition="fade-transition">Coś poszło nie
+                    tak. Popraw dane
+                    logowania i spróbuj ponownie
+                </v-alert>
+                <div class="headline text-xs-center">Zaloguj się</div>
                 <v-form @submit.prevent="login">
                     <v-text-field prepend-icon="person" name="Username" label="Login"
                                   v-model="username"></v-text-field>
                     <v-text-field prepend-icon="lock" name="Password" label="Hasło" type="password"
                                   v-model="password"></v-text-field>
+                    <v-layout>
+                        <v-flex xs6>
+                            <v-btn block type="submit">Zaloguj</v-btn>
+                        </v-flex>
+                        <v-flex xs6>
+                            <v-btn block v-on:click="register">Zarejestruj się</v-btn>
+                        </v-flex>
+                    </v-layout>
 
-                    <v-btn type="submit">Zaloguj</v-btn>
-                    <v-btn v-on:click="register">Zarejestruj się</v-btn>
                 </v-form>
             </v-flex>
         </v-layout>
@@ -27,7 +38,13 @@
             return {
                 username: '',
                 password: '',
-                grant_type: 'password'
+                grant_type: 'password',
+                errors: []
+            }
+        },
+        computed: {
+            hasErrors() {
+                return !!this.errors.length
             }
         },
         methods: {
@@ -36,10 +53,15 @@
                 this.$store.dispatch(AUTH_REQUEST, {grant_type, username, password})
                     .then(() => {
                         this.$router.push('/')
-                    })
+                    }).catch(err => {
+                    this.errors = [...this.errors, err]
+                })
             },
             register() {
                 this.$router.push('/register')
+            },
+            closeAlert() {
+                this.errors = []
             }
         }
 
