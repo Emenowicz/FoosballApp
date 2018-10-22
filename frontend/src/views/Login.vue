e6865b7f056f4cddda6064f4a7e78570e51cbf58
Dawid Michalowicz <dmichalowicz96@gmail.com>
2018-10-22 23:12:33 +0200

    login validation

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -11,9 +11,11 @@
                 <div class="headline text-xs-center">Zaloguj się</div>
                 <v-form @submit.prevent="login">
                     <v-text-field prepend-icon="person" name="Username" label="Login"
-                                  v-model="username"></v-text-field>
+                                  v-model="username" :error-messages="usernameErrors" @input="$v.username.$touch()"
+                                  @blur="$v.username.$touch()"></v-text-field>
                     <v-text-field prepend-icon="lock" name="Password" label="Hasło" type="password"
-                                  v-model="password"></v-text-field>
+                                  v-model="password" :error-messages="passwordErrors" @input="$v.password.$touch()"
+                                  @blur="$v.password.$touch()"></v-text-field>
                     <v-layout>
                         <v-flex xs6>
                             <v-btn block type="submit">Zaloguj</v-btn>
@@ -22,7 +24,6 @@
                             <v-btn block v-on:click="register">Zarejestruj się</v-btn>
                         </v-flex>
                     </v-layout>
-
                 </v-form>
             </v-flex>
         </v-layout>
@@ -32,6 +33,7 @@
 
 <script>
     import {AUTH_REQUEST} from "../store/actions/authentication";
+    import required from "vuelidate/src/validators/required";
 
     export default {
         name: "Login",
@@ -46,18 +48,33 @@
         computed: {
             hasErrors() {
                 return !!this.errors.length
+            },
+            usernameErrors() {
+                const errors = []
+                if (!this.$v.username.$dirty) return errors
+                !this.$v.username.required && errors.push('Login jest wymagany')
+                return errors
+            },
+            passwordErrors() {
+                const errors = []
+                if (!this.$v.password.$dirty) return errors
+                !this.$v.password.required && errors.push('Hasło jest wymagane')
+                return errors
             }
         },
         methods: {
             login() {
-                this.errors = []
-                const {grant_type, username, password} = this
-                this.$store.dispatch(AUTH_REQUEST, {grant_type, username, password})
-                    .then(() => {
-                        this.$router.push('/')
-                    }).catch(err => {
-                    this.errors = [...this.errors, err]
-                })
+                this.$v.$touch()
+                if (!this.$v.$anyError) {
+                    this.errors = []
+                    const {grant_type, username, password} = this
+                    this.$store.dispatch(AUTH_REQUEST, {grant_type, username, password})
+                        .then(() => {
+                            this.$router.push('/')
+                        }).catch(err => {
+                        this.errors = [...this.errors, err]
+                    })
+                }
             },
             register() {
                 this.$router.push('/register')
